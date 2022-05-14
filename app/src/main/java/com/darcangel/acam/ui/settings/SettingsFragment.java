70301316aa7050faafd76b050d120890dbaeb2a7
James Turner <thursdaynext@gmail.com>
2022-05-14 04:17:08 -0700

    fixed databinding callback
    added setModel

--- app/src/main/java/com/darcangel/acam/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/darcangel/acam/ui/settings/SettingsFragment.java
@@ -2,13 +2,19 @@ package com.darcangel.acam.ui.settings;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.databinding.adapters.TextViewBindingAdapter;
+import androidx.databinding.adapters.TextViewBindingAdapter.OnTextChanged;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.darcangel.acam.databinding.FragmentSettingsBinding;
@@ -19,12 +25,25 @@ public class SettingsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         SettingsViewModel settingsViewModel =
                 new ViewModelProvider(this).get(SettingsViewModel.class);
-        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        binding.emissivity.setText("98.6");
+        binding.setModel(settingsViewModel);
+
+        //Create the observer
+        final Observer<String> emissivityObserver = new Observer<String>() {
+            @Override
+            public void onChanged(String value) {
+                Log.d("SettingsFragment", "Emissivity is " + value);
+            }
+        };
+        settingsViewModel.getEmissivity().observe(this, emissivityObserver);
 
         return root;
     }
--- app/src/main/java/com/darcangel/acam/ui/settings/SettingsViewModel.java
+++ app/src/main/java/com/darcangel/acam/ui/settings/SettingsViewModel.java
@@ -1,19 +1,35 @@
 package com.darcangel.acam.ui.settings;
 
+import android.util.Log;
+
+import androidx.databinding.adapters.TextViewBindingAdapter;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SettingsViewModel extends ViewModel {
+public class SettingsViewModel extends ViewModel  {
 
-    private final MutableLiveData<String> mText;
+    public MutableLiveData<String> emissivity;
 
     public SettingsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is settings fragment");
+        emissivity = new MutableLiveData<String>();
+        emissivity.setValue("98.6");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public MutableLiveData<String> getEmissivity() {
+        if(emissivity == null) {
+            emissivity = new MutableLiveData<String>();
+        }
+        return emissivity;
+    }
+
+    public void setEmissivity(String value) {
+        emissivity.setValue(value);
+    }
+
+
+    public void onEmissivityTextChanged(CharSequence text) {
+        Log.d("SettingsFragment", "Emissivity is " + text);
+        setEmissivity(text.toString());
     }
 }
