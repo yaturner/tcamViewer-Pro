c0ef1e4bfce0ec04a7231c5b4c809ef386016acc
James Turner <thursdaynext@gmail.com>
2022-08-28 06:21:30 -0700

    added error handler

--- tcamViewer/src/main/java/com/darcangel/tcamViewer/Application.java
+++ tcamViewer/src/main/java/com/darcangel/tcamViewer/Application.java
@@ -1,16 +1,48 @@
 package com.darcangel.tcamViewer;
 
+import java.io.IOException;
+
 import dagger.hilt.android.HiltAndroidApp;
+import io.reactivex.rxjava3.exceptions.UndeliverableException;
+import io.reactivex.rxjava3.plugins.RxJavaPlugins;
 import timber.log.Timber;
 
 @HiltAndroidApp
 public class Application extends android.app.Application {
-    //private DaggerController mDaggerController;
-
     public void onCreate() {
         super.onCreate();
-        //mDaggerController = new DaggerController(this);
         initTimber();
+
+        RxJavaPlugins.setErrorHandler(e -> {
+            if (e instanceof UndeliverableException) {
+                e = e.getCause();
+            }
+            if (e instanceof IOException) {
+                // fine, irrelevant network problem or API that throws on cancellation
+                Timber.e(e);
+                return;
+            }
+            if (e instanceof InterruptedException) {
+                // fine, some blocking code was interrupted by a dispose call
+                Timber.e(e);
+                return;
+            }
+            if ((e instanceof NullPointerException) || (e instanceof IllegalArgumentException)) {
+                // that's likely a bug in the application
+                Thread.currentThread().getUncaughtExceptionHandler().
+                        uncaughtException(Thread.currentThread(), e);
+                Timber.e(e);
+                return;
+            }
+            if (e instanceof IllegalStateException) {
+                // that's a bug in RxJava or in a custom operator
+                Thread.currentThread().getUncaughtExceptionHandler()
+                        .uncaughtException(Thread.currentThread(), e);
+                Timber.e(e);
+                return;
+            }
+            Timber.d(e, "Undeliverable exception received, not sure what to do");
+        });
     }
 
     private void initTimber() {
